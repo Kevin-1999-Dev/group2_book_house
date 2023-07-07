2f627fa4fa2a5e7b0e424414742d6217ea19281e
Peter Zam <mail@peterzam.dev>
2023-07-07 04:43:32 +0000

    add active default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             'address' => 'Knowhere',
             'password' => bcrypt('admin'),
             'role' => true,
+            'active' => true,
         ]);
 
         User::create([
