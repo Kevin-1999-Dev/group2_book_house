c222090971f705ddb9d0abbf547bcaed5a41e5c0
Peter Zam <mail@peterzam.dev>
2023-07-07 04:18:20 +0000

    move timestamps to bottom

--- database/migrations/4_order.php
+++ database/migrations/4_order.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('comment')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
