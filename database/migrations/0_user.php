423289e437e346d6215aa897456dba3e25e82242
Peter Zam <mail@peterzam.dev>
2023-07-07 04:49:18 +0000

    remove active

--- database/migrations/0_user.php
+++ database/migrations/0_user.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('profile_photo_path', 2048)->nullable();
             $table->string('address');
             $table->boolean('role');
-            $table->boolean('active');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,6 @@ class DatabaseSeeder extends Seeder
             'address' => 'Knowhere',
             'password' => bcrypt('admin'),
             'role' => true,
-            'active' => true,
         ]);
 
         User::create([
