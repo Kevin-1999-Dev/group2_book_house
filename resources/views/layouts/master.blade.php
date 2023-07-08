8f3043a8cad7891ae4a044dca8df2ecc12ea8e11
Mal Sawm Daung Sangi <daungmikrokosmos@gmail.com>
2023-07-08 10:27:41 +0630

    navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,39 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-    {{-- Css --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    {{-- bootstrap css --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    {{-- bootstrap bundle --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    {{-- google fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  {{-- Css --}}
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  {{-- bootstrap css --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  {{-- bootstrap bundle --}}
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  {{-- google fonts --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <section class="shadow-sm">
-        <header class="container">
-          <div class="cmn-inner row d-flex justify-content-center align-items-center">
-            <h1 class="col-6">
-                <img src="{{ asset('images/img_bookhouse_logo.png') }}" alt="comida">
-                 Book House
-            </h1>
-            <nav class="col-2 offset-4">
-             <a href="{{ route('auth.loginPage') }}" class="text-decoration-none">
-                <button class="btn btn-primary">Loign</button>
-             </a>
-             <a href="{{ route('auth.registerPage') }}" class="text-decoration-none">
-                <button class="btn btn-primary">Register</button>
-             </a>
-            </nav>
-          </div>
-        </header>
-      </section><!-- about-us-head -->
+  @include('layouts.nav')
+
+  @yield('content')
+
 </body>
+
 </html>
