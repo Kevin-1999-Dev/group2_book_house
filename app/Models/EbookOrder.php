edf6c4e2b24e5e587934bb6282d0e2c0ec4ff469
Peter Zam <mail@peterzam.dev>
2023-07-25 17:47:03 +0000

    format fix models

--- app/Models/EbookOrder.php
+++ app/Models/EbookOrder.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * EbookOrder
+ */
 class EbookOrder extends Model
 {
     use HasFactory;
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Order
+ */
 class Order extends Model
 {
     use HasFactory;
